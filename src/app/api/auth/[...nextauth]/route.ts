d33293e377edad74527512fb0c17d9bbce215ac8
Keanu Reaño <reanokeanu@gmail.com>
2023-08-09 14:45:57 +0800

    [gac] feat: implement NextAuth authentication and user session handling This commit adds authentication functionality using NextAuth and sets up user session handling. It introduces a new file route.ts under the [...nextauth] route, which defines authentication options using the CredentialsProvider from NextAuth. The provided credentials (email and password) are sent to a Spring Boot login API for verification, and the user object is returned upon successful authentication. Additionally, the commit includes changes to the layout and page components. The layout.tsx file now wraps the main content with a NextAuthProvider, which ensures that user sessions are managed for authenticated users. The page.tsx file has been updated to display user information using the User component from the new components directory. This allows rendering user session data on the client side. The providers.tsx file introduces a NextAuthProvider component that encapsulates the SessionProvider from next-auth/react, providing a centralized way to manage user sessions across the application. Overall, this commit enhances the application's security by implementing authentication and session management, improving the user experience by showing user-specific content based on their session status. Please note that this commit message is generated based on the provided Git diff log and may need further refinement based on the actual changes and their significance.

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,41 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        // Call your Spring Boot login API here
+        console.log(credentials);
+
+        const res = await fetch("http://localhost:8080/api/auth/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(credentials),
+        });
+
+        const user = await res.json();
+
+        if (res.ok && user) {
+          // Transform your user object as needed
+          console.log(user);
+          return Promise.resolve(user);
+        } else {
+          return Promise.resolve(null);
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
