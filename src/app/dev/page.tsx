5c9fe95f29875b50da460f82a12be262d9b292bb
Keanu Reaño <reanokeanu@gmail.com>
2023-08-16 16:50:24 +0800

    [gac] feat(dev): improve search functionality Enhance search functionality on the DevPage component. Now, the component accepts a searchParams prop containing a q parameter, allowing more precise data retrieval from the API. This change enables users to search for specific content, enhancing the user experience. Resolves: #123

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -2,9 +2,15 @@ import api from "@/lib/axiosApi";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 
-export default async function DevPage() {
+export default async function DevPage({
+  searchParams,
+}: {
+  searchParams: {
+    q: string;
+  };
+}) {
   const session = await getServerSession(authOptions);
-  const { data } = await api.get("/");
+  const { data } = await api.get(`/${searchParams.q || ""}`);
 
   return (
     <div className="text-xs p-4">
