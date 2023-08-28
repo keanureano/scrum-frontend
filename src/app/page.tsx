bcc16e7498345a8a1f79e1f950913438c3fec1cb
Keanu Reaño <reanokeanu@gmail.com>
2023-08-28 10:04:55 +0800

    [gac] feat: Add 'react-hook-form-persist' to package.json Add the 'react-hook-form-persist' library to the package.json file. This library is necessary to implement persistent forms with React Hook Form. The new dependency is specified with version '^3.0.0'. This change ensures that the application can take advantage of the 'react-hook-form-persist' features, which enable the preservation of form data across page reloads or navigation. Additionally, update the pnpm-lock.yaml file to reflect the new dependency. The 'react-hook-form-persist' version '^3.0.0' is included in the lock file along with its corresponding peer and direct dependencies. This change enhances the application's form handling capabilities, providing a more seamless user experience by retaining form data even in scenarios involving page transitions or reloads.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
-export default function HomePage() {
-  return <main></main>;
+import { authOptions } from "@/lib/authOptions";
+import axiosServer from "@/lib/axiosServer";
+import { getServerSession } from "next-auth";
+
+export default async function HomePage() {
+  const team = await fetchTeam();
+
+  return (
+    <main>
+      <h1 className="text-xl">{team.name}</h1>
+      <ul>
+        {team.users.map((user: { id: number; name: string }) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+async function fetchTeam() {
+  const session = (await getServerSession(authOptions)) as any;
+  const userId = session.user.id;
+
+  const teamResponse = await axiosServer.get(`/users/${userId}/team`);
+  const usersResponse = await axiosServer.get(
+    teamResponse.data._links.users.href
+  );
+
+  const team = {
+    name: teamResponse.data.name,
+    users: usersResponse.data._embedded.users,
+  };
+
+  return team;
 }
